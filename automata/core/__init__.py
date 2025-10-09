1851617b523212681efc62b3450a17925730595d
Fridemn <702625325@qq.com>
2025-10-09 13:51:19 +0800

    chore: remove submodule imports to prevent circular dependencies

--- automata/core/__init__.py
+++ automata/core/__init__.py
@@ -1,7 +1,1 @@
 # Core package
-
-from . import db
-from .managers.context_mgr import ContextManager
-from .managers.conversation_mgr import ConversationManager
-from .managers.message_history_mgr import MessageHistoryManager
-from .server.web_server import AutomataDashboard
